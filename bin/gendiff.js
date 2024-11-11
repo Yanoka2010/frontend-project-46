5212239e8745abeae854631b275ccea1b133aaf3
Yanoka2010 <yanasakrotenko1@gmail.com>
2024-11-11 15:12:18 +0300

    hh

---
+++ bin/gendiff.js
@@ -0,0 +1,11 @@
+import { Command } from "commander";
+const program = new Command();
+
+program
+    .description('Compares two configuration files and shows a difference.')
+    .version('0.8.0')
+    .option('-f, --format [type]', 'output format')
+    .argument('<filepath1>')
+    .argument('<filepath2>')
+
+program.parse();
