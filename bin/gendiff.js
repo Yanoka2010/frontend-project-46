90e2e261d60c0b628c7f37ea2ccdba8f6ea83482
Yanoka2010 <yanasakrotenko1@gmail.com>
2024-11-29 15:04:29 +0300

    step3-4

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,18 @@
+#!/usr/bin/env node
 import { Command } from "commander";
+import parseFile from "../src/parsers.js";
+import genDiff from "../src/diff.js";
+
 const program = new Command();
 
 program
+    .argument('<filepath1>')
+    .argument('<filepath2>')
     .description('Compares two configuration files and shows a difference.')
     .version('0.8.0')
     .option('-f, --format [type]', 'output format')
-    .argument('<filepath1>')
-    .argument('<filepath2>')
+    .action((filepath1, filepath2) => {
+        console.log(genDiff(parseFile(filepath1), parseFile(filepath2)))
+    });
 
 program.parse();
