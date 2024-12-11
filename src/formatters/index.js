2e4d3ababc303b069d800ac459f5105d4721eae1
Yanoka2010 <yanasakrotenko1@gmail.com>
2024-12-11 15:12:54 +0300

    step 9

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,12 +1,15 @@
 import { stylish } from './stylish.js';
 import { plain } from './plain.js';
 import { createDiff } from '../utils.js';
+import json from './json.js';
 
 const format = (data1, data2, formatter) => {
     const diff = createDiff(data1, data2);
     switch (formatter) {
       case 'plain':
         return plain(diff).trim();
+      case 'json':
+        return json(diff);
       default:
         return stylish(diff);
     }
